661edf8d12f19a17d9b2e63411113638fbb265a6
Germán Ramos <german.ramos@lightit.io>
2025-10-29 10:44:35 -0300

    feat: Employer Factory

--- database/factories/EmployerFactory.php
+++ database/factories/EmployerFactory.php
@@ -3,12 +3,11 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use App\Models\Employer;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Job>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Employer>
  */
-class JobFactory extends Factory
+class EmployerFactory extends Factory
 {
     /**
      * Define the model's default state.
@@ -18,9 +17,7 @@ class JobFactory extends Factory
     public function definition(): array
     {
         return [
-            'title' => fake()->jobTitle(),
-            'employer_id' => Employer::factory(),
-            'salary' => '$50.000 USD',
+            'name' => fake()->company(),
         ];
     }
 }
