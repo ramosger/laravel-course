90c0abe6b2b7bf9388c659aa0d613ae664ea3240
Germán Ramos <german.ramos@lightit.io>
2025-10-29 17:55:47 -0300

    feat: Update && Delete Methods

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -3,9 +3,13 @@
         Job Page
     </x-slot:heading>
 
-    <h2>{{ $job['title'] }}</h2>
+    <h2>{{ $job->title }}</h2>
     <p class="font-bold text-lg">
-        This Job pays ${{ $job['salary'] }} per year.
+        This Job pays ${{ $job->salary }} per year.
+    </p>
+
+    <p class="mt-6">
+        <x-button href="/jobs/{{ $job->id }}/edit">Edit Job</x-button>
     </p>
 
 </x-layout>
