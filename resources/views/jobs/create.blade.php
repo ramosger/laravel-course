957936f68a30540df6c8952ad5856fc583791da5
Germán Ramos <german.ramos@lightit.io>
2025-10-29 16:25:30 -0300

    feat: Csrf Usage

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -4,6 +4,7 @@
     </x-slot:heading>
 
     <form method="POST" action="/jobs">
+        @csrf
         <div class="space-y-12">
             <div class="border-b border-gray-900/10 pb-12">
                 <h2 class="text-base/7 font-semibold text-gray-900">Create a New Job</h2>
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ Route::get('/about', function() {
 });
 
 Route::get('/jobs', function() {
-    $jobs = Job::with('employer')->cursorPaginate(3);
+    $jobs = Job::with('employer')->latest()->cursorPaginate(3);
 
     return view('jobs.index', ['jobs' => $jobs]);
 });
@@ -28,7 +28,13 @@ Route::get('/jobs/{id}', function($id) {
 });
 
 Route::post('/jobs', function() {
-    dd('Hello Broder');
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 1,
+    ]);
+
+    return redirect('/jobs');
 });
 
 Route::get('/contact', function() {
