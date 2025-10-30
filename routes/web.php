67941684e405e29c4f4b420032c02040c5d74dfa
Germán Ramos <german.ramos@lightit.io>
2025-10-30 09:36:48 -0300

    feat: Route Resource

--- routes/web.php
+++ routes/web.php
@@ -4,17 +4,7 @@ use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
-
 Route::view('/about', 'about');
-
 Route::view('/contact', 'contact');
 
-Route::controller(JobController::class)->group(function () {
-    Route::get('/jobs', 'index');
-    Route::get('/jobs/create', 'create');
-    Route::get('/jobs/{job}', 'show');
-    Route::post('/jobs', 'store');
-    Route::get('/jobs/{job}/edit', 'edit');
-    Route::patch('/jobs/{job}', 'update');
-    Route::delete('/jobs/{job}', 'destroy');
-});
+Route::resource('jobs', JobController::class);
