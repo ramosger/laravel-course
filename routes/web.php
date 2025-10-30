7ea74b27766c984f2b370513b556acaf0d777ad6
Germán Ramos <german.ramos@lightit.io>
2025-10-30 09:31:39 -0300

    feat: Route View & Listing

--- routes/web.php
+++ routes/web.php
@@ -3,22 +3,18 @@
 use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::view('/', 'home');
 
-Route::get('/about', function () {
-    return view('about');
-});
+Route::view('/about', 'about');
 
-Route::get('/contact', function () {
-    return view('contact');
-});
+Route::view('/contact', 'contact');
 
-Route::get('/jobs', [JobController::class, 'index']);
-Route::get('/jobs/create', [JobController::class,'create']);
-Route::get('/jobs/{job}', [JobController::class,'show']);
-Route::post('/jobs', [JobController::class,'store']);
-Route::get('/jobs/{job}/edit', [JobController::class,'edit']);
-Route::patch('/jobs/{job}', [JobController::class,'update']);
-Route::delete('/jobs/{job}', [JobController::class,'destroy']);
+Route::controller(JobController::class)->group(function () {
+    Route::get('/jobs', 'index');
+    Route::get('/jobs/create', 'create');
+    Route::get('/jobs/{job}', 'show');
+    Route::post('/jobs', 'store');
+    Route::get('/jobs/{job}/edit', 'edit');
+    Route::patch('/jobs/{job}', 'update');
+    Route::delete('/jobs/{job}', 'destroy');
+});
