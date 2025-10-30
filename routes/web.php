f2620b82988ff764211eceead69db33253180189
Germán Ramos <german.ramos@lightit.io>
2025-10-30 09:19:58 -0300

    refactor: JobController

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
-use App\Models\Job;
 
 Route::get('/', function () {
     return view('home');
@@ -11,59 +11,14 @@ Route::get('/about', function () {
     return view('about');
 });
 
-Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->latest()->cursorPaginate(3);
-
-    return view('jobs.index', ['jobs' => $jobs]);
-});
-
-Route::get('/jobs/create', function () {
-    return view('jobs.create');
-});
-
-Route::get('/jobs/{job}', function (Job $job) {
-    return view('jobs.show', ['job' => $job]);
-});
-
-Route::post('/jobs', function () {
-    request()->validate([
-        'title' => ['required', 'min:3'],
-        'salary' => ['required'],
-    ]);
-
-    Job::create([
-        'title' => request('title'),
-        'salary' => request('salary'),
-        'employer_id' => 1,
-    ]);
-
-    return redirect('/jobs');
-});
-
-Route::get('/jobs/{job}/edit', function (Job $job) {
-    return view('jobs.edit', ['job' => $job]);
-});
-
-Route::patch('/jobs/{job}', function (Job $job) {
-    request()->validate([
-        'title' => ['required', 'min:3'],
-        'salary' => ['required'],
-    ]);
-
-    $job->update([
-        'title' => request('title'),
-        'salary' => request('salary'),
-    ]);
-
-    return redirect('/jobs/' . $job->id);
-});
-
-Route::delete('/jobs/{job}', function (Job $job) {
-    $job->delete();
-
-    return redirect('/jobs');
-});
-
 Route::get('/contact', function () {
     return view('contact');
 });
+
+Route::get('/jobs', [JobController::class, 'index']);
+Route::get('/jobs/create', [JobController::class,'create']);
+Route::get('/jobs/{job}', [JobController::class,'show']);
+Route::post('/jobs', [JobController::class,'store']);
+Route::get('/jobs/{job}/edit', [JobController::class,'edit']);
+Route::patch('/jobs/{job}', [JobController::class,'update']);
+Route::delete('/jobs/{job}', [JobController::class,'destroy']);
